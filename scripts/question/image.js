12b19e2c798465dc4edc7f020501b472dad39c21
Nqbral <sim.chevalier@gmail.com>
2023-10-17 14:43:05 +0200

    [#40] - Fix path images

---
+++ scripts/question/image.js
@@ -0,0 +1,44 @@
+class QuestionImage extends Question {
+  /**
+   * Constructor QuestionImage
+   *
+   * @param {string} imageSrc image source
+   * @param {string} themeQuestion theme of the question
+   * @param {array[string]} answers list of possible answers
+   */
+  constructor(imageSrc, themeQuestion, answers) {
+    super(themeQuestion, answers);
+
+    this.imgSrc = imageSrc;
+  }
+
+  /**
+   * Init the html code to display the question content
+   *
+   * @returns Element
+   */
+  initHtmlQuestionContent() {
+    let element = document.createElement("img");
+
+    element.src =
+      "../../assets/images/" + this.themeQuestion + "/" + this.imgSrc;
+
+    return element;
+  }
+
+  /**
+   * String for the question
+   *
+   * @returns string
+   */
+  getQuestion() {
+    switch (this.themeQuestion) {
+      case "anime":
+        return "De quel anime provient cette image ?";
+      case "movie":
+        return "De quel film provient cette image ?";
+      default:
+        return "De quelle série provient cette image ?";
+    }
+  }
+}
